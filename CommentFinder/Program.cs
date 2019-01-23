33a9a5d1baedcaa67f3277fcdfe81d1259db814b
Kevin Huangfu <kevinzaft@gmail.com>
2019-01-23 11:32:45 -0500

    Add project files.

---
+++ CommentFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommentFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
